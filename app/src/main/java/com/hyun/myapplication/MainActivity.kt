d94d5509d55e3f5d129321ce0d84fb59d3261992
SeungHyunBaek <back947@naver.com>
2019-07-27 14:02:38 +0900

    Create MyHomeActivity(BottomNavigationView)

--- app/src/main/java/com/hyun/myapplication/MainActivity.kt
+++ app/src/main/java/com/hyun/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hyun.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Intent(this, MyHomeActivity::class.java).let {
+            startActivity(it)
+        }
     }
 }
