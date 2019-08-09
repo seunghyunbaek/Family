794af22e63e1cc77ed23a9ed1c9fda370b70ff75
SeungHyunBaek <back947@naver.com>
2019-08-09 18:25:29 +0900

    Create Data Model

---
+++ app/src/main/java/com/hyun/myapplication/model/Record.kt
@@ -0,0 +1,6 @@
+package com.hyun.myapplication.model
+
+class Record(val name:String, val date: String) {
+    lateinit var text:String
+    lateinit var img:Array<String>
+}
---
+++ app/src/main/java/com/hyun/myapplication/model/Todo.kt
@@ -0,0 +1,6 @@
+package com.hyun.myapplication.model
+
+class Todo {
+    lateinit var title:String
+    lateinit var date: String
+}
